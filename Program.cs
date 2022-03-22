3b100aed31993b271a01aaddbacc6ff287df8bba
Nikolina Dokšanović <65850108+nopacak@users.noreply.github.com>
2022-03-22 11:22:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string charachterName = "Joseph";
+int characterAge = 36;
+
+Console.WriteLine("There once was a man named " + charachterName);
+Console.WriteLine("He was " + characterAge + " years old");
+Console.WriteLine("He really liked the name " + charachterName);
+Console.WriteLine("But didn't like being " + characterAge);
+
+Console.ReadLine();
