da60614b10532d61ff2cb40067d91888b9add063
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-22 14:14:12 +0100

    Add files via upload

---
+++ AreaOfTheCircle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            //your code goes here
+
+            Console.WriteLine(pi * radius * radius);
+        }
+    }
+}
