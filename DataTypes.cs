7d1a530bbd1f1e71ab91434bdf894b818f9c6e07
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-22 11:58:01 +0100

    Add files via upload

---
+++ DataTypes.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string phrase = "We found a hat";
+char city = 'W';
+int count = 5;
+float radius = 3;
+double circumference = 2;
+decimal price = 5.79M;  //The M tells the compiler that 5.79 is a decimal
+bool isBrown = false;
+
+Console.WriteLine(phrase + " at " + city + ".");
+Console.ReadLine();
+
