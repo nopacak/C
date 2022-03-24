1c7f104e02b6cbd133ef02626de94c4e6ea9e638
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 12:56:57 +0100

    Add files via upload

---
+++ WhileLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (++num < 6)
+                Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
