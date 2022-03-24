87e58bb5a01d506864ccdd2c6ae04fd2d2507e6a
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 10:52:19 +0100

    Add files via upload

---
+++ Strings.cs
@@ -0,0 +1,10 @@
+﻿String phrase = "First Line\nSecond Line";
+
+//Console.WriteLine(phrase.Length);
+//Console.WriteLine(phrase.ToUpper());
+//Console.WriteLine(phrase.Contains("line"));
+//Console.WriteLine(phrase[15]);
+//Console.WriteLine(phrase.IndexOf('w'));
+Console.WriteLine(phrase.Substring(6,7));
+Console.ReadLine();
+
