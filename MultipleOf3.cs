8d342d32e6f53064fed35fc8cc3a8cdec868a671
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 15:08:18 +0100

    Add files via upload

---
+++ MultipleOf3.cs
@@ -0,0 +1,13 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+//your code goes here
+for (int i = 1; i <= N; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine('*');
+        continue;
+    }
+
+
+    Console.WriteLine(i);
