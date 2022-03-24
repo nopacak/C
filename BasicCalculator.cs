2f0ecc20fd21b2214d9d819aeef9d0ae22d1401f
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 13:26:39 +0100

    Add files via upload

---
+++ BasicCalculator.cs
@@ -0,0 +1,13 @@
+﻿do
+{
+    Console.Write("x = ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("y = ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    int sum = x + y;
+    Console.WriteLine("Result: {0}", sum);
+}
+while (true);
+Console.ReadLine();
