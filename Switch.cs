4106cb866b61ac2117872e62e64310590e448634
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 12:37:47 +0100

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,17 @@
+﻿int num = 15;
+switch (num)
+{
+    case 1:
+        Console.WriteLine("one");
+        break;
+    case 2:
+        Console.WriteLine("two");
+        break;
+    case 3:
+        Console.WriteLine("three");
+        break;
+    default:
+        Console.WriteLine("The default case");
+        break;
+}
+Console.ReadLine();
