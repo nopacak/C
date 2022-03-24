7a65ca3799500408b3adcfa9ba6b4c2ad8e6046d
Nikolina Opačak <65850108+nopacak@users.noreply.github.com>
2022-03-24 13:31:24 +0100

    Update BasicCalculator.cs

--- BasicCalculator.cs
+++ BasicCalculator.cs
@@ -1,7 +1,25 @@
-﻿do
+/*do
 {
     Console.Write("x = ");
     int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("y = ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    int sum = x + y;
+    Console.WriteLine("Result: {0}", sum);
+}
+while (true);
+Console.ReadLine();*/
+
+do
+{
+    Console.Write("x = ");
+    string str = Console.ReadLine();
+    if (str == "exit")
+        break;
+
+    int x = Convert.ToInt32(str);
 
     Console.Write("y = ");
     int y = Convert.ToInt32(Console.ReadLine());
